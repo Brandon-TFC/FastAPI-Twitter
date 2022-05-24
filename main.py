742211fec7775d3ff3955d9fdd663bec65f0c342
Brandon <tfcbrandong@gmail.com>
2022-05-23 19:40:04 -0500

    tweet_post

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ class Tweet(BaseModel):
         max_length = 256
     )
     create_at: datetime = Field(default=datetime.now())
-    update_at: Optional[datetime] = Field(default=None)
+    updated_at: Optional[datetime] = Field(default=None)
     by: User = Field(...)
 #Path operations
 
@@ -203,8 +203,37 @@ def home():
     tags=["Tweets"]
 )
 
-def post():
-    pass
+def post(tweet: Tweet = Body(...)):
+    """
+    Post a tweet
+    
+    This path operation post a tweet in the app
+
+    parameters:
+        -Request body parameter
+        -user: tweet
+    
+    Returns a json with the basic tweet information
+        tweet_id: UUID 
+        content: str
+        created_at: datetime 
+        updated_at: Optional[datetime]
+        by: User
+    """
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        tweet_dict= tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])#Esto lo hacemos para los archivos uuid y date, poder almacenarlos en un json
+        tweet_dict["create_at"] = str(tweet_dict["create_at"])  #Esto lo hacemos para los archivos uuid y date, poder almacenarlos en un json
+        tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+        tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"]) #accedemos al by mediante las llaves y despues a su respectiva llave que deseemos convertir el uudi
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])#accedemos al by mediante las llaves y despues a su respectiva llave que deseemos convertir el uudi
+        
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet
+
 
 ###Show a tweets
 @app.get(
