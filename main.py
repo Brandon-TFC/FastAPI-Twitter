7404072d0cdf5c849406621377f4556543073171
Brandon <tfcbrandong@gmail.com>
2022-05-23 19:12:03 -0500

    users_show_all

--- main.py
+++ main.py
@@ -125,7 +125,22 @@ def login():
 )
 
 def show_all_users():
-    pass
+    """
+    This path operation shows all users in the app
+
+    parameters:
+        -
+
+    Returns a json list with all users in the app, with the following keys
+        -user_id: uuid
+        -email: Emailstr
+        -first_name: str
+        -last_name: str
+        -birth_date: datetime
+    """
+    with open("users.json", "r",encoding="utf-8") as f:
+        results = json.loads(f.read())
+        return results
 
 
 ###show_a_user
